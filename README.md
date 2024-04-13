11ca64953874bd6c85deed872686c3993ddb0241
Jörg Franke <frankej@cs.uni-freiburg.de>
2024-04-13 23:00:44 +0200

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+# GPR - Generative Pretrained Regression
+____
+
+other names:
+GPSR - Generative Pretrained Symbolic Regression
+PSR - Pretrained Symbolic Regression
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import shutil
+from distutils.core import setup
+
+setup(name='gpr',
+      version='0.0.1',
+      packages=['gpr'],
+
+      dependency_links=[],
+      )
