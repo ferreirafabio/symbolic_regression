e826baf7ca4994b133d8c8c98f42ab5893d87007
Jörg Franke <frankej@cs.uni-freiburg.de>
2024-05-03 12:25:45 +0200

    add training loop and training code fix minor bugs in data loader changed batch to dict add config file

--- RUN.sh
+++ RUN.sh
@@ -1,12 +1,12 @@
 
 
 
-sbatch --nodes=1 --account=cstdl --partition=dc-gpu-devel --time=0:30:00 slurm_launch_juwels.sh "experiment.session_name=design_model
-experiment.experiment_name=test_setup_256-6_bs32_rel200
-model.model_dim=768
-model.num_head=6
+sbatch --nodes=1 --account=cstdl --partition=dc-gpu-devel --time=2:00:00 slurm_launch_juwels.sh "experiment.session_name=design_model
+experiment.experiment_name=test_setup_512-6_bs8_rel200
+model.model_dim=512
+model.num_head=4
 model.n_layers=8
-data_simple.num_realisations=200
+data_simple.num_realisations=100
 data_simple.val_samples=500
-data_simple.batch_size=32
-optim.lr=0.001"
+data_simple.batch_size=8
+optim.lr=0.0003"
