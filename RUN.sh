09db784f6fdd31b2b803e385f46a5ab91301584c
Jörg Franke <frankej@cs.uni-freiburg.de>
2024-05-14 21:48:42 +0200

    run

--- RUN.sh
+++ RUN.sh
@@ -6,7 +6,28 @@ experiment.experiment_name=test_setup_1024-12_bs4x4x8_rel200
 model.model_dim=1024
 model.num_head=8
 model.n_layers=12
-data_simple.num_realisations=100
+data_simple.num_realisations=200
 data_simple.val_samples=500
 data_simple.batch_size=16
 optim.lr=0.0003"
+
+
+sbatch --nodes=4 --account=projectnucleus --partition=booster --time=10:00:00 slurm_launch_juwels.sh "experiment.session_name=first_hpo_1
+experiment.experiment_name=setup_1024-12_bs4x4x16_rel200_lr0003
+model.model_dim=1024
+model.num_head=8
+model.n_layers=12
+data_simple.num_realisations=200
+data_simple.val_samples=500
+data_simple.batch_size=16
+optim.lr=0.0003"
+
+sbatch --nodes=4 --account=projectnucleus --partition=booster --time=10:00:00 slurm_launch_juwels.sh "experiment.session_name=first_hpo_1
+experiment.experiment_name=setup_1024-12_bs4x4x16_rel200_lr001
+model.model_dim=1024
+model.num_head=8
+model.n_layers=12
+data_simple.num_realisations=200
+data_simple.val_samples=500
+data_simple.batch_size=16
+optim.lr=0.001"
