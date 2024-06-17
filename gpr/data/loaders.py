f58512150c612ec4da8aa5d38155cc8645b8e872
ferreirafabio <ferreirafabio@users.noreply.github.com>
2024-06-17 17:33:13 +0200

    fixed the seeding problem where setting the seed with multiple workers would lead to identical equations across batches

--- gpr/data/loaders.py
+++ gpr/data/loaders.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import torch
 import yaml
-
+import random
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from numpy.random import default_rng
@@ -17,13 +17,14 @@ class SymPySimpleDataModule(object):
     def __init__(self, generator, config_path):
         global_config = Config(config_file=config_path)
         self.config = global_config.dataloader
-        self.seed = self.config.generator.seed
-        self.generator = generator(rng=default_rng(self.seed))
-        self.rng = self.generator.rng
+        self.worker_seeds = np.zeros(self.config.num_workers, dtype=int)
+        self.seed = self.config.generator.seed # base seed for training set
+        self.val_seed = self.seed + 1000  # base seed for validation set
+        self.generator_class = generator
+        self.generators = [None] * self.config.num_workers
 
         self.ignore_index = -100
         self.pad_index = 0
-        self.validation_set = self.create_validation_set()
 
     def get_vocab(self):
         return characters
@@ -46,19 +47,21 @@ class SymPySimpleDataModule(object):
                 batch[key] = value.to(device)
         return batch
 
-    def create_sample(self, seed=None):
+    def create_sample(self, is_validation=False):
         """Return a tbl of n inference samples of the equation, and the target
         token sequnce of the latex equation."""
-        if seed is not None:
-            self.generator.rng = default_rng(seed=seed)
-        return self.generator(**self.config.generator)
-
-    def create_validation_set(self):
-        validation_data = [self.create_sample() for _ in range(self.config.val_samples)]
-        validation_dataset = EquationDataset(
-            data_source=validation_data,
-        )
-        return validation_dataset
+        worker_info = torch.utils.data.get_worker_info()
+        if worker_info is not None:
+            worker_id = worker_info.id
+            if self.generators[worker_id] is None:
+                raise RuntimeError(f"Generator for worker {worker_id} is not initialized.")
+            generator = self.generators[worker_id]
+        else:
+            # num_workers <= 1 or valid dataloader case
+            seed = self.val_seed if is_validation else self.seed
+            rng = default_rng(seed)
+            generator = self.generator_class(rng=rng)
+        return generator(**self.config.generator)
 
     def collator(self, batch):
         """get a set of samples. return a batch for tbl and trg_tex. pad target
@@ -78,24 +81,48 @@ class SymPySimpleDataModule(object):
                 "equation": [item[3] for item in batch],
                 'trg_len': torch.tensor([len(item[2]) for item in batch])}
 
+    def worker_init_fn(self, worker_id):
+        self.worker_seeds[worker_id] = self.config.generator.seed + worker_id
+        seed = self.worker_seeds[worker_id]
+        rng = default_rng(seed)
+        self.generators[worker_id] = self.generator_class(rng=rng)
+        torch.cuda.manual_seed(seed)
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+        # print(f"worker {worker_id} has seed: {seed}")
+
+    def worker_init_fn_validation(self, worker_id):
+        seed = self.val_seed + worker_id
+        rng = default_rng(seed)
+        self.generators[worker_id] = self.generator_class(rng=rng)
+        torch.cuda.manual_seed(seed)
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+        # print(f"validation worker {worker_id} has seed: {seed}")
+        
     def get_train_loader(self):
         """return a dataloader over an infinite set of training data."""
         train_dataset = EquationDataset(
-            data_source=self.create_sample, # Pass the function to generate samples on-the-fly
+            data_source=self.create_sample,
         )
         train_loader = DataLoader(train_dataset,
                                   batch_size=self.config.batch_size,
                                   collate_fn=self.collator,
-                                  num_workers=self.config.num_workers)
+                                  num_workers=self.config.num_workers,
+                                  worker_init_fn=self.worker_init_fn,
+                                )
         return train_loader
 
     def get_valid_loader(self):
-        """return a dataloader over a finite set of validation data, which was
-        created in the create_validation_set method."""
-        valid_loader = DataLoader(self.validation_set,
+        valid_dataset = EquationDataset(
+                    data_source=self.create_sample,
+                )
+        valid_loader = DataLoader(valid_dataset,
                                   batch_size=self.config.batch_size,
                                   collate_fn=self.collator,
-                                  num_workers=self.config.num_workers)
+                                  num_workers=self.config.num_workers,
+                                  worker_init_fn=self.worker_init_fn_validation,
+                                  )
         return valid_loader
 
 
@@ -108,14 +135,21 @@ if __name__ == "__main__":
     valid_loader = sympy_data.get_valid_loader()
 
     print("Validation equations:")
-    for batch_valid in valid_loader:
-        print(f"equation batch {batch_valid['equation']}")
+    counter = 0
+    for batch in valid_loader:
+        print(f"Batch {counter} equations:")
+        for equation in batch['equation']:
+            print(equation)
+        counter += 1
+        if counter == 5:
+            break
 
     print("Training equations:")
     counter = 0
     for batch in train_loader:
-        print(f"equation batch {batch_valid['equation']}")
-        #print(f"equation batch {batch_valid['mantissa']}")
+        print(f"Batch {counter} equations:")
+        for equation in batch['equation']:
+            print(equation)
         counter += 1
         if counter == 5:
             break
