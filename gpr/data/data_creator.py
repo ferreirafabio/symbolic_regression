c56709f59e2dd448adf08f4c02f58723a0000b6c
Jörg Franke <frankej@cs.uni-freiburg.de>
2024-08-14 23:06:12 +0200

    WIP: print dataloader output

---
+++ gpr/data/data_creator.py
@@ -0,0 +1,144 @@
+import os
+import pathlib
+
+import sympy as sp
+import pandas as pd
+import numpy as np
+import torch
+import yaml
+import random
+import logging
+from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
+from numpy.random import default_rng
+
+from gpr.data.datasets import EquationDataset
+from gpr.data.utils import all_tokens
+from gpr.utils.configuration import Config
+from sympy.parsing.latex import parse_latex
+from gpr.data.utils import tokenize_latex_to_char
+
+
+class CreateDataset(object):
+    def __init__(self, generator, config_path, exp_folder):
+        global_config = Config(config_file=config_path)
+        self.config = global_config.dataloader
+        self.worker_seeds = np.zeros(self.config.num_workers, dtype=int)
+        self.seed = self.config.generator.seed  # base seed for training set
+        self.val_seed = self.seed + 1000  # base seed for validation set
+
+        self.generator_class = generator
+        self.generators = [None] * self.config.num_workers
+
+        self.worker_seeds[0] = self.config.generator.seed + 0
+        seed = self.worker_seeds[0]
+        rng = default_rng(seed)
+        self.generators[0] = self.generator_class(rng=rng)
+        torch.cuda.manual_seed(seed)
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+
+
+
+        rng = default_rng(self.seed)
+        self.generator = self.generator_class(rng=rng)
+
+        self.equation_logger = logging.getLogger('equation_logger')
+        self.equation_logger.setLevel(logging.INFO)
+        fh = logging.FileHandler(exp_folder / 'equation_log.txt')
+        fh.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        self.equation_logger.addHandler(fh)
+
+        # Prevent the equation_logger from propagating to the root logger
+        self.equation_logger.propagate = False
+
+        self.ignore_index = -100
+        self.pad_index = 0
+
+    def get_vocab(self):
+        return all_tokens
+
+    def indices_to_string(self, indices):
+        if isinstance(indices, torch.Tensor):
+            indices = indices.tolist()
+        return ''.join([all_tokens[i] for i in indices])
+
+    @property
+    def vocab_size(self):
+        return len(self.get_vocab())
+
+
+
+
+
+    def create_sample(self):
+        sample = self.generator(**self.config.generator)
+
+        return sample
+
+
+    def create_set(self):
+        dataset = []
+        for _ in range(self.config.val_samples):
+            mantissa, exponent, expression = self.create_sample()
+            self.equation_logger.info(f"Validation equation: {expression}")
+            latex_expression = sp.latex(expression)
+            latex_token_indices = tokenize_latex_to_char(latex_expression)
+            token_tensor = torch.tensor(latex_token_indices, dtype=torch.long)
+            dataset.append((mantissa, exponent, token_tensor))
+
+        return dataset
+
+
+    def collator(self, batch):
+        """get a set of samples. return a batch for tbl and trg_tex. pad target
+        sequence with ignore index and input table with pad index."""
+        mantissa_stack = pad_sequence([item[0].t() for item in batch],
+                                      batch_first=True,
+                                      padding_value=self.pad_index).transpose(2,1)
+        exponent_stack = pad_sequence([item[1].t() for item in batch],
+                                      batch_first=True,
+                                      padding_value=self.pad_index).transpose(2,1).to(mantissa_stack.dtype)
+        latex_token_stack = pad_sequence([item[2] for item in batch],
+                                         batch_first=True,
+                                         padding_value=self.pad_index)
+        return {"mantissa": mantissa_stack,
+                "exponent": exponent_stack,
+                "latex_token": latex_token_stack,
+                'trg_len': torch.tensor([len(item[2]) for item in batch])}
+
+
+
+
+    def get_valid_loader(self):
+        """return a dataloader over an infinite set of training data."""
+        train_dataset = self.create_set()
+
+        train_loader = DataLoader(train_dataset,
+                                  batch_size=self.config.batch_size,
+                                  collate_fn=self.collator,
+                                  )
+        return train_loader
+
+
+
+if __name__ == "__main__":
+    from gpr.data.generators import RandomGenerator, PolynomialGenerator
+
+    sympy_data = CreateDataset(generator=PolynomialGenerator,
+                                       config_path='config/data_config.yaml', exp_folder=pathlib.Path('.'))
+    valid_loader = sympy_data.get_valid_loader()
+
+    print("Validation equations:")
+    counter = 0
+    for batch in valid_loader:
+        print(f"Batch {counter} equations:")
+        for equation in batch['latex_token']:
+            print(equation)
+        counter += 1
+        if counter == 5:
+            break
+
+
--- gpr/data/loaders.py
+++ gpr/data/loaders.py
@@ -4,17 +4,44 @@ import numpy as np
 import torch
 import yaml
 import random
+import logging
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from numpy.random import default_rng
 
 from gpr.data.datasets import EquationDataset
-from gpr.data.utils import characters
+from gpr.data.utils import all_tokens
 from gpr.utils.configuration import Config
+from sympy.parsing.latex import parse_latex
+
+
+def clean_variable_names(equation):
+    """Replace subscripted variable names with non-subscripted names."""
+    lhs, rhs = equation.lhs, equation.rhs
+    var_map = {}
+    counter = 1
+
+    # Sort variables to ensure consistent ordering
+    sorted_vars = sorted(rhs.free_symbols, key=lambda v: str(v))
+
+    for var in sorted_vars:
+        var_str = str(var)
+        if var_str == 'x':
+            # if x and x_1 are present in predicted eq., do not use x_1 for x
+            # since true eq. can have an x_1 at the same position of x_1 in predicted eq.
+            var_map[var] = sp.Symbol('x')
+        else:
+            # For any other variables, including x_{i}, remove '_' and use x{counter}
+            new_var = sp.Symbol(f'x{counter}')
+            var_map[var] = new_var
+            counter += 1
+
+    new_rhs = rhs.subs(var_map)
+    return sp.Eq(lhs, new_rhs), var_map
 
 
 class SymPySimpleDataModule(object):
-    def __init__(self, generator, config_path):
+    def __init__(self, generator, config_path, exp_folder):
         global_config = Config(config_file=config_path)
         self.config = global_config.dataloader
         self.worker_seeds = np.zeros(self.config.num_workers, dtype=int)
@@ -23,29 +50,105 @@ class SymPySimpleDataModule(object):
         self.generator_class = generator
         self.generators = [None] * self.config.num_workers
 
+        self.equation_logger = logging.getLogger('equation_logger')
+        self.equation_logger.setLevel(logging.INFO)
+        fh = logging.FileHandler(exp_folder / 'equation_log.txt')
+        fh.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        self.equation_logger.addHandler(fh)
+
+        # Prevent the equation_logger from propagating to the root logger
+        self.equation_logger.propagate = False
+
         self.ignore_index = -100
         self.pad_index = 0
 
     def get_vocab(self):
-        return characters
+        return all_tokens
 
 
     def indices_to_string(self, indices):
         if isinstance(indices, torch.Tensor):
             indices = indices.tolist()
-        return ''.join([characters[i] for i in indices])
+        return ''.join([all_tokens[i] for i in indices])
 
     @property
     def vocab_size(self):
         return len(self.get_vocab())
 
     def latex_equation_to_function(self, latex_equation):
-        #TODO
-        pass
+        """Convert a LaTeX equation string to a SymPy function."""
+        try:
+            parsed_eq = self.parse_latex_equation(latex_equation)
 
-    def check_if_latex_equation_is_valid(self):
-        #TODO
-        return True
+            # clean variable names from underscored variables
+            cleaned_eq, var_map = clean_variable_names(parsed_eq)
+            variables = [var_map.get(var, var) for var in cleaned_eq.free_symbols if str(var) != 'y']
+
+            return cleaned_eq, variables
+
+        except Exception as e:
+            self.equation_logger.info(f"Failed to parse LaTeX equation: {latex_equation}. Error: {e}")
+            return None, None
+
+    def parse_latex_equation(self, latex_equation):
+        """Check if a LaTeX equation string is valid by attempting to parse it."""
+
+        # Quick check for balanced braces
+        if latex_equation.count('{') != latex_equation.count('}'):
+            raise ValueError("Unbalanced braces in LaTeX equation")
+
+        try:
+            parsed_eq = parse_latex(latex_equation)
+            return parsed_eq
+
+        except Exception as e:
+            raise e
+
+    def compute_mse(self, predicted_seq, true_seq):
+        """Compute the mean squared error between two latex equations."""
+        pred_eq, pred_vars = self.latex_equation_to_function(predicted_seq)
+        true_eq, true_vars = self.latex_equation_to_function(true_seq)
+
+        # check if equations are valid
+        if pred_eq is None or true_eq is None:
+            return float('inf')
+
+        self.equation_logger.info(f"pred_vars: {pred_vars} from pred_eq: {pred_eq} (latex string: {predicted_seq})")
+        self.equation_logger.info(f"true_vars: {true_vars} from true_eq: {true_eq} (latex string: {true_seq})")
+
+        # Generate a union of all variables
+        all_vars = list(set(pred_vars) | set(true_vars))
+
+        # Generate values for variables
+        # var_values = {(var): np.linspace(-10, 10, 50) for var in all_vars}
+        var_values = {var: np.random.uniform(-10, 10, 10) for var in all_vars}
+
+        try:
+            pred_input_values = [var_values[(var)] for var in pred_vars]
+            true_input_values = [var_values[(var)] for var in true_vars]
+
+            # Log variable values
+            for var in pred_vars:
+                self.equation_logger.info(f"Values for {var} in pred_eq: {var_values[var]}")
+            for var in true_vars:
+                self.equation_logger.info(f"Values for {var} in true_eq: {var_values[var]}")
+
+             # Create lambdified functions and evaluate them
+            pred_func = sp.lambdify([var for var in pred_vars], pred_eq.rhs, modules=['numpy'])
+            true_func = sp.lambdify([var for var in true_vars], true_eq.rhs, modules=['numpy'])
+
+            pred_values = pred_func(*pred_input_values)
+            true_values = true_func(*true_input_values)
+
+            mse = np.mean((pred_values - true_values) ** 2)
+            self.equation_logger.info(f"pred_values: {pred_values}, true_values: {true_values}, MSE: {mse}")
+            return mse
+        except Exception as e:
+            self.equation_logger.info(f"Failed to evaluate equations: {predicted_seq} and {true_seq}")
+            self.equation_logger.info(f"Error: {str(e)}")
+            return float('inf')
 
     def batch_to_device(self, batch, device):
         for key, value in batch.items():
