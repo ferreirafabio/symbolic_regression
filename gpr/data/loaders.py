3b073b1acc825790476fc8a9467285dec2bfc09a
Jörg Franke <frankej@cs.uni-freiburg.de>
2024-09-05 12:15:43 +0200

    Merge branch 'main' of github.com:JoergFranke/ScalingSymbolicRegression
    # Conflicts:
    #	gpr/data/loaders.py

--- gpr/data/loaders.py
+++ gpr/data/loaders.py
@@ -1,3 +1,4 @@
+import collections
 from typing import List, Tuple
 from collections import defaultdict
 from functools import partial
@@ -18,8 +19,8 @@ from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
 
 from gpr.data.utils import all_tokens, token_to_index
-from gpr.data.data_creator import get_base_name, get_base_name_test
-
+from gpr.data.data_creator import get_base_name
+from gpr.data.test_data_creator import get_base_name_test
 
 # TODO multi gpu, multi node loading
 # TODO samller realization as in data arraw file
@@ -159,12 +160,64 @@ class SymPySimpleDataModule(object):
                 'trg_len': torch.tensor([seq.shape[0] + 1 for seq in batch['token_tensor']])}
 
 
+    def index_pa_sets_collator(self, indices, datasets_lookup, num_realizations):
+        """get a set of samples. return a batch for tbl and trg_tex. pad target
+        sequence with ignore index and input table with pad index."""
+
+        batch = defaultdict(list)
+
+        for i in indices:
+
+            pre_i = 0
+            for dataset, dataset_size in datasets_lookup.items():
+                if i < dataset_size:
+                    break
+                pre_i = dataset_size
+            i = i - pre_i
+
+            sample = dataset.get_record_batch(i)
+            for key in ['mantissa', 'exponent', 'token_tensor']:
+                array_data = np.array(sample[key].to_pylist()[0])
+                tensor_data = torch.from_numpy(array_data)
+                if key == 'mantissa':
+                    tensor_data = tensor_data[:num_realizations, :]
+                    tensor_data = tensor_data.float().round(decimals=self.real_const_decimal_places).to(torch.float16)
+
+                elif key == 'exponent':
+                    tensor_data = tensor_data[:num_realizations, :]
+                    tensor_data = tensor_data.to(torch.int8)
+
+                batch[key].append(tensor_data.t())  # Transpose for PyTorch
+
+            # batch['latex_expression'].append(sample['latex_expression'].to_pylist()[0])
+
+        mantissa_stack = pad_sequence(batch['mantissa'],
+                                      batch_first=True,
+                                      padding_value=self.pad_index).transpose(2,1)
+        exponent_stack = pad_sequence(batch['exponent'],
+                                      batch_first=True,
+                                      padding_value=self.pad_index).transpose(2,1).to(mantissa_stack.dtype)
+
+        input_seq = [torch.cat([torch.tensor([self.soe_index], dtype=torch.uint8), tokens]) for tokens in batch['token_tensor']]
+        input_seq_stack = pad_sequence(input_seq, batch_first=True, padding_value=self.pad_index)
+
+        target_seq = [torch.cat([tokens, torch.tensor([self.eoe_index], dtype=torch.uint8)]) for tokens in batch['token_tensor']]
+        target_seq_stack = pad_sequence(target_seq, batch_first=True, padding_value=self.ignore_index)
+
+        return {"mantissa": mantissa_stack,
+                "exponent": exponent_stack,
+                "in_equation": input_seq_stack,
+                "trg_equation": target_seq_stack,
+                # "latex_equation": batch['latex_expression'],
+                'trg_len': torch.tensor([seq.shape[0] + 1 for seq in batch['token_tensor']])}
+
+
     def get_data_loader(self, set_name: str):
         """return a dataloader over an finite set of training data."""
 
         assert set_name in ['train', 'valid', 'test']
 
-        if self.config.project_name is not None:
+        if "project_name" not in self.config or self.config.project_name is None:
             if set_name in ['train', 'valid']:
                 base_name = get_base_name(self.config, set_name)
             else:
@@ -182,6 +235,24 @@ class SymPySimpleDataModule(object):
                     f"Data file '{file_dir}' not found. Run the data creation script first.\n"
                     f"Available files in '{data_dir}':\n{available_files_str}"
                 )
+
+            mmap = pa.memory_map(file_dir)
+            self.logger.info("MMAP Read ALL")
+            dataset = pa.ipc.open_file(mmap)
+
+            self.logger.info(f"DataLoader for {set_name} successfully loaded from {file_dir}.")
+
+            train_set_size = dataset.num_record_batches
+            self.logger.info(f"Number of {set_name} samples: {train_set_size}")
+
+            indices = list(range(train_set_size))
+            indices = np.random.permutation(indices)
+            index_dataset = IndexDataset(indices)
+
+            train_pl_collate_fn = partial(self.index_pa_collator, dataset=dataset,
+                                          num_realizations=self.config.generator.num_realizations)
+
+
         else:
             data_dir = pathlib.Path(self.config.data_dir)
             project_dir = (data_dir / self.config.project_name).as_posix()
@@ -195,24 +266,29 @@ class SymPySimpleDataModule(object):
             if len(arrow_files) == 0:
                 raise FileNotFoundError(f"No files found for {set_name} in {project_dir}")
 
+            datasets = []
+            dataset_size = []
+            for file_dir in arrow_files:
+                self.logger.info(f"Loading file: {file_dir}")
+                mmap = pa.memory_map(file_dir)
+                self.logger.info("MMAP Read ALL")
+                dataset = pa.ipc.open_file(mmap)
+                datasets.append(dataset)
 
+                file_samples = dataset.num_record_batches
+                self.logger.info(f"Number of samples in {file_dir}: {file_samples}")
+                dataset_size.append(file_samples)
 
+            self.logger.info(f"DataLoader for {set_name} successfully loaded from {file_dir}.")
 
+            indices = list(range(sum(dataset_size)))
+            indices = np.random.permutation(indices)
+            index_dataset = IndexDataset(indices)
 
-        mmap = pa.memory_map(file_dir)
-        self.logger.info("MMAP Read ALL")
-        dataset = pa.ipc.open_file(mmap)
+            cum_dataset_size = np.cumsum(dataset_size).tolist()
+            datasets_lookup = collections.OrderedDict({d:n for d, n in zip(datasets, cum_dataset_size)})
 
-        self.logger.info(f"DataLoader for {set_name} successfully loaded from {file_dir}.")
-
-        train_set_size = dataset.num_record_batches
-        self.logger.info(f"Number of {set_name} samples: {train_set_size}")
-
-        indices = list(range(train_set_size))
-        indices = np.random.permutation(indices)
-        index_dataset = IndexDataset(indices)
-
-        train_pl_collate_fn = partial(self.index_pa_collator, dataset=dataset, num_realizations=self.config.generator.num_realizations)
+            train_pl_collate_fn = partial(self.index_pa_sets_collator, datasets_lookup=datasets_lookup, num_realizations=self.config.generator.num_realizations)
 
         data_loader = DataLoader(
             index_dataset,
@@ -262,7 +338,13 @@ class SymPySimpleDataModule(object):
 
 if __name__ == "__main__":
 
-    sympy_data = SymPySimpleDataModule(config_path='config/default_config.yaml', exp_folder='exp')
+    from gpr.utils.configuration import Config
+
+    config_path = 'config/default_config'
+
+    global_config = Config(config_file=config_path)
+
+    sympy_data = SymPySimpleDataModule(global_config, None)
     train_loader = sympy_data.get_train_loader()
     valid_loader = sympy_data.get_valid_loader()
     test_loader = sympy_data.get_test_loader()
@@ -276,18 +358,16 @@ if __name__ == "__main__":
         print(f"in_equation: {batch['in_equation']}")
         print(f"trg_equation: {batch['trg_equation']}")
         print(f"trg_len: {batch['trg_len']}")
-        for equation in batch['equation']:
-            print(equation)
         counter += 1
         if counter == 5:
             break
 
-    print("Training equations:")
-    counter = 0
-    for batch in train_loader:
-        print(f"Batch {counter} equations:")
-        for equation in batch['equation']:
-            print(equation)
-        counter += 1
-        if counter == 5:
-            break
+    # print("Training equations:")
+    # counter = 0
+    # for batch in train_loader:
+    #     print(f"Batch {counter} equations:")
+    #     for equation in batch['equation']:
+    #         print(equation)
+    #     counter += 1
+    #     if counter == 5:
+    #         break
