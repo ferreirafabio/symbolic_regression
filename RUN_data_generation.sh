106c69fa4d186d7e8ec5cd94c1336cbce5241e88
Jörg Franke <frankej@cs.uni-freiburg.de>
2024-09-07 08:07:42 +0200

    Generate eq WIP

--- RUN_data_generation.sh
+++ RUN_data_generation.sh
@@ -3,10 +3,18 @@
 
 
 
+pip install antlr4-python3-runtime==4.11
 
 
 
 
+sbatch -p bosch_cpu-cascadelake -nodes=1 -time=4:00:00
+srun -p bosch_cpu-cascadelake --nodes=1 --time=1:00:00 --cpus-per-task=32 --bosch --pty bash
 
-sbatch -p bosch_cpu-cascadelake -nodes=1 -time=1:00:00
-srun -p bosch_cpu-cascadelake --nodes=1 --time=1:00:00  --bosch --pty bash
+/home/frankej/workspace/ScalingSymbolicRegression/venv/bin/python gpr/data/data_creator.py -f -c feynman_arc_config data_dir="/mhome/frankej/workspace/data"
+
+/home/frankej/workspace/ScalingSymbolicRegression/venv/bin/python gpr/data/data_creator.py -f -c feynman_arc_config data_dir="/mhome/frankej/workspace/data"
+
+
+
+sbatch --time=1:00:00 slurm_data_gen_kislurm.sh -c feynman_arc_config data_dir="/mhome/frankej/workspace/test_data"
---
+++ slurm_data_gen_kislurm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+#SBATCH --nodes=1
+#SBATCH -p bosch_cpu-cascadelake
+#SBATCH --cpus-per-task=32
+#SBATCH --job-name=gpr_d
+#SBATCH --time=2-00:00:00
+#SBATCH --output=/home/frankej/experiments/symreg/output/mpi-out.%j
+#SBATCH --error=/home/frankej/experiments/symreg/error/mpi-err.%j
+
+cuda12.1
+
+echo "KISLURM Job"
+source /home/frankej/workspace/ScalingSymbolicRegression/venv/bin/activate
+
+PYTHON_SCRIPT=/home/frankej/workspace/ScalingSymbolicRegression/gpr/data/data_creator.py
+
+srun python $PYTHON_SCRIPT -f $@
+
